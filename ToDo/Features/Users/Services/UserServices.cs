cdcc1d4fbd0751b411d8844ee0022a41a4dd7a19
EnaNguyen <nguyenquangquyx@gmail.com>
2026-01-19 08:31:05 +0700

    remove bin

--- ToDo/Features/Users/Services/UserServices.cs
+++ ToDo/Features/Users/Services/UserServices.cs
@@ -12,35 +12,10 @@ namespace ToDo.Features.Users.Services
             _context = context;
             _mapper = mapper;
         }
-        public Task<UserView> ChangePasswordAsync(UserChangePasswordDTO userChangePasswordDTO, int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserView> CreateUserAsync(UserCreateDTO userCreateDTO)
-        {
-            throw new NotImplementedException();
-        }
-
         public async Task<List<UserView>> GetAllUsersAsync()
         {
             var _users = _context.Users.ToList();
             return _mapper.Map<List<UserView>>(_users);
-        }
-
-        public Task<UserView> GetUserAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RemoveUserAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<UserView> UpdateUserAsync(UserCreateDTO userUpdateDTO, int id)
-        {
-            throw new NotImplementedException();
         }
     }
 }
